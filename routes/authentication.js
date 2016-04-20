0d1b310c2b0deb52350ab960e59610744c669cc7
Sagar Gangabisoensingh <sagartje1993@gmail.com>
2016-04-20 16:28:59 +0200

    return logged in user

--- routes/authentication.js
+++ routes/authentication.js
@@ -35,6 +35,14 @@ module.exports = function(frontEndRouter, passport){
         failureRedirect : '/login'
     }));
     
+    frontEndRouter.route('/profile')
+    .get(function(req, res){
+        if(req.isAuthenticated()){
+            res[200](req.user);
+        } else {
+            res[400]('user not logged in')
+        }
+    });
     
     //log out
     frontEndRouter.route('/logout')
